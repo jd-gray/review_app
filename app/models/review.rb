27d1bf96e8f3fe29060fdd5636e853e3caddaf77
Jared Gray <jared@jaredgray.us>
2018-06-03 09:56:54 -0700

    Add functonality in review model to increment/decrement total rating in meal
    If review is negative, then a comment must exist

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review < ApplicationRecord
 	belongs_to :user
 	belongs_to :meal
+
+  after_save :update_meal_rating_count
+
+  validates :comment, presence: true, if: -> { !liked }
+
+  private
+
+  def update_meal_rating_count
+    liked ? meal.increment!(:total_rating) : meal.decrement!(:total_rating)
+  end
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -22,4 +22,35 @@ class ReviewTest < ActiveSupport::TestCase
     refute review.valid?
     assert_not_nil review.errors[:meal]
   end
+
+  test "negative review must have a comment" do
+    review = Review.new(liked: false, comment: nil, meal: @meal, user: @user)
+    assert review.invalid?
+
+    review = Review.new(liked: false, comment: 'Food is bad', meal: @meal, user: @user)
+    assert review.valid?
+  end
+
+  test "positive review updates total rating for meal" do
+    review = Review.create!(liked: true, comment: nil, meal: @meal, user: @user)
+
+    assert_equal 1, @meal.total_rating
+  end
+
+  test "negative review updates total rating for meal" do
+    review = Review.create!(liked: false, comment: 'Food is bad', meal: @meal, user: @user)
+
+    assert_equal -1, @meal.total_rating
+  end
+
+  test "multiple reviews update total rating for meal" do
+    review1 = Review.create!(liked: true, comment: nil, meal: @meal, user: create(:user))
+    assert_equal 1, @meal.total_rating
+
+    review2 = Review.create!(liked: true, comment: nil, meal: @meal, user: create(:user))
+    assert_equal 2, @meal.total_rating
+
+    review3 = Review.create!(liked: false, comment: 'Food is bad', meal: @meal, user: create(:user))
+    assert_equal 1, @meal.total_rating
+  end
 end
